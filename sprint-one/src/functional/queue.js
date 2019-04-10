65b3358b937e7b97c7d2401e5f246bfde8e83f86
bjlagman12 <bjlagman@gmail.com>
2019-04-10 11:47:05 -0700

    Implement for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,41 @@
 var Queue = function() {
-  var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  var someInstance = {};
+  // declare a counter to keep track of the queue
+  var counter = 0;
+  // declare a counter to keep track of the removed ones
+  var remove = 0;
 
   someInstance.enqueue = function(value) {
+    // increment counter
+    counter ++;
+    // add to the storage
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    // set a conditional so that the remove is larger than 0
+    if (remove >= 0) {
+      // increment remove counter
+      remove ++;
+      // create a variable for what we are dequeing
+      var rem = storage[remove];
+      // remove from the storage
+      delete rem;
+      return rem;
+    }
+    // need to return what we are removing
   };
 
   someInstance.size = function() {
+    // set a condition to see if counter is larger than the removed
+    if (counter >= remove) {
+      // return counter minus the removed counter
+      return counter - remove;
+      // need a conditional if rmemove was pressed more than once
+    } else if (remove > 0) {
+      return 0;
+    }
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,36 @@
 var Stack = function() {
-  var someInstance = {};
-
-  // Use an object with numeric keys to store values
   var storage = {};
+  var someInstance = {};
+  // use a counter to identify where we stand in the position
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // increment counter
+    counter ++;
+    // store in the storage with the key representing the counter and the value as a value
+    storage[counter] = value;
+
   };
 
   someInstance.pop = function() {
+
+    // need to set a variable for what we are removing
+    var remove = storage[counter];
+    // set a conditional if the counter is greater than 0
+    if (counter > 0) {
+      // decrement counter 
+      counter --;
+      // delete the key from the storage
+      delete remove;
+      // return what we removed
+      return remove;
+    }
   };
 
   someInstance.size = function() {
+    // return the counter since it will represend our size
+    return counter;
   };
 
   return someInstance;
