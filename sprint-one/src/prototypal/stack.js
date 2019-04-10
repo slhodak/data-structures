8ae13b2233b57fa8aa04c026d534f708df209800
slhodak <slhodak@gmail.com>
2019-04-10 13:14:51 -0700

    Fixed prototypal stack; does not extend constructor's prototype

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.top = 0;
+  newStack.storage = {};
+  
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(data) {
+  this.storage[this.top] = data;
+  this.top++;
+};
 
+stackMethods.pop = function() {
+  if (this.top) {
+    this.top--;
+    return this.storage[this.top];
+  }
+};
+
+stackMethods.size = function() {
+  return this.top;
+};
