d92a5792a66919dc8b6a8d6d4a439e702dfa2997
slhodak <slhodak@gmail.com>
2019-04-10 13:20:02 -0700

    Implemented pseudoclassical patterns of class instantiation for stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.end = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(data) {
+  this.storage[this.end] = data;
+  this.end++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.end) {
+    var firstInLine = this.storage[0];
+    for (var i = 0; i < this.end; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    this.end--;
+    return firstInLine;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.end;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(data) {
+  this.storage[this.top] = data;
+  this.top++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.top) {
+    this.top--;
+    return this.storage[this.top];
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.top;
+};
 
