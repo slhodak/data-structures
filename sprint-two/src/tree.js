1edc4ba97d2ca36f9c6daf4b8d05ee11bb0ef7e4
slhodak <slhodak@gmail.com>
2019-04-10 20:42:15 -0700

    Implement tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree = _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +11,37 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+/*
+create a new tree variable with the value
+add the new tree to the calling object children
+*/
+var node = Tree(value);
+this.children.push(node);
+
 };
 
 treeMethods.contains = function(target) {
+/*
+check if current nodes value is equal to target
+  if true return true
+iterate through the children
+  apply recursion for each child
+  check to see if the following condition is true
+   if true => this.children[i].contains(target)
+   return true
+return false
+*/
+if (this.value === target) {
+  return true;
+}
+for (var i = 0; i < this.children.length; i++ ) {
+  if (this.children[i].contains(target)) {
+    return true;
+  }
+}
+return false;
+
+
 };
 
 
