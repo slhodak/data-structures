304f2aaccb98fac4337695e246acea966f765962
slhodak <slhodak@gmail.com>
2019-04-12 18:13:52 -0700

    Created html page for profiling functions

---
+++ sprint-two/spec/profiling.js
@@ -0,0 +1,22 @@
+//  Linked List
+//  create a large number of linked lists
+//  add a few elements to all of them
+//  use contains on all
+
+//  Tree
+
+
+
+//  Graph
+
+
+
+//  Set
+
+
+
+//  Hash Table
+
+
+
+//  Binary Search Tree
